38d6c0ed47a09b837b9c817e17c6d3bb85bd1ea9
sirlyluhfi <sirlyluthfi05@gmail.com>
2017-10-23 09:59:38 +0700

    menampilkan luas rumah dan luas box parket

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -5,6 +5,7 @@ int main(){
 	double LRumah = 42;
 	double lantaiparket = 193*1215;
 	double L1Box = lantaiparket*8/1000000;
-	
+	cout << "luas rumah : "<<LRumah<< " m2"<<endl;
+	cout << "luas 1 box parket : "<<L1Box<<"m2"<<endl;
 }
 
