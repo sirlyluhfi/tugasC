ebfcfd3304b1984d5d02cb1a254e428213a4f49f
sirlyluhfi <sirlyluthfi05@gmail.com>
2017-10-23 10:16:09 +0700

    biaya yang harus di keluarkan
    menghitung dan menampilkan
    luas rumah
    luas 1 box
    jumlah bok P
    Biaya P
    biaya pasang
    total biaya

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -7,5 +7,15 @@ int main(){
 	double L1Box = lantaiparket*8/1000000;
 	cout << "luas rumah : "<<LRumah<< " m2"<<endl;
 	cout << "luas 1 box parket : "<<L1Box<<"m2"<<endl;
+	double jumlahBoxP,BiayaP,BiayaPG,totalB;
+	jumlahBoxP = LRumah/L1Box;
+	cout << " jumlah box parket : "<<jumlahBoxP<<endl;
+	BiayaP = jumlahBoxP * 500;
+	cout << "Biaya parket (Rp) : "<<BiayaP<<endl;
+	BiayaPG = LRumah * 20;
+	cout << "Biaya pasang(Rp) : "<<BiayaPG<<endl;
+	totalB = BiayaP+BiayaPG;
+	cout << "Total Biaya(Rp)  : "<<totalB<<endl; 
+	
 }
 
